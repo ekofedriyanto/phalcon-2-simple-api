2b1c33b46838cb20abd58f162c898da8d0e21451
Eko Fedriyanto <echo.firstson@gmail.com>
2015-05-03 02:44:01 +0700

    First Init

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+* @Author: Eko Fedriyanto
+* @Date:   2015-05-03 02:38:32
+* @Last Modified by:   Owner
+* @Last Modified time: 2015-05-03 02:43:45
+* @File name: index.php
+**/
+
+echo '<pre>';
+print_r(get_loaded_extensions());
