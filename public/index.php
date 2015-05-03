01de4092fa67181e289983968a72df2e1813899d
Eko Fedriyanto <echo.firstson@gmail.com>
2015-05-03 17:20:09 +0700

    Reinit

--- public/index.php
+++ public/index.php
@@ -4,9 +4,10 @@
 * @Author: Eko Fedriyanto
 * @Date:   2015-05-03 02:38:32
 * @Last Modified by:   Owner
-* @Last Modified time: 2015-05-03 02:43:45
+* @Last Modified time: 2015-05-03 17:19:47
 * @File name: index.php
 **/
 
 echo '<pre>';
 print_r(get_loaded_extensions());
+echo '</pre>';
